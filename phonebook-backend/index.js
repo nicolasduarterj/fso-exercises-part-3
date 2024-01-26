65e860876a9122295289139b211d9aa5bee81976
nicolasduarterj <nicolas_duarterj@proton.me>
2024-01-26 17:49:58 -0300

    First build to render

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 app = express()
 app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
 morgan.token('body' , function (req, res) {return JSON.stringify(req.body)})
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body', {
   skip: function (req, res) {return req.method !== "POST"}
