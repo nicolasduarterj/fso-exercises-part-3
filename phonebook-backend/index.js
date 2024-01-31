ab718058efb28b4ed4c02dedaa61ff999aaf586c
nicolasduarterj <nicolas_duarterj@proton.me>
2024-01-31 20:53:54 -0300

    Exercises 3.15 -> 3.18

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -14,12 +14,11 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
 
 
 
-app.get('/info', (req, res) => {
-  Person.find({}).then(persons => {
-    const data = new Date().toString()
-    const pageRes = `<p>Phonebook has info for ${persons.length} people</p><p>${data}</p>`
-    res.send(pageRes) 
-  })
+app.get('/info', async (req, res) => {
+  const persons = await Person.find({})
+  const data = new Date().toString()
+  const pageRes = `<p>Phonebook has info for ${persons.length} people</p><p>${data}</p>`
+  res.send(pageRes) 
 })
 
 app.get('/api/persons', (req, res) => {
@@ -28,13 +27,18 @@ app.get('/api/persons', (req, res) => {
     })
 })
 
-app.get('/api/persons/:id', (req, res) => {
+app.get('/api/persons/:id', (req, res, next) => {
   const id = req.params.id
-  let validos = []
-  Person.findById(id).then(response => {
-    validos = response
-    res.json(validos)
-  })
+  Person.findById(id).then(pers => {
+    if (pers !== null) {
+      console.log(pers)
+      const validos = pers
+      res.json(validos) 
+    }
+    else {
+      res.status(404).end()
+    }
+  }).catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (req, res) => {
@@ -43,7 +47,7 @@ app.delete('/api/persons/:id', (req, res) => {
   res.status(204).end()
 })
 
-app.post('/api/persons', (req, res) => {
+app.post('/api/persons', (req, res, next) => {
   const body = req.body
   if (!body.name || !body.number) {
     res.statusMessage = 'missing values OR that person already exists on the database'
@@ -55,10 +59,43 @@ app.post('/api/persons', (req, res) => {
       number: body.number
     })
     person.save().then(console.log("Person saved"))
+      .catch(error => next(error))
     res.json(person)
   }
 })
 
+app.put('/api/persons/:id', async (req, res, next) => {
+  const body = req.body
+
+  const newpers = {
+    name: body.name,
+    number: body.number
+  }
+  
+  try {
+    const updatedPers = await Person.findByIdAndUpdate(req.params.id, newpers, {new: true})
+    res.json(updatedPers)
+  }
+  catch(error) {
+    next(error)
+  }
+})
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({error: 'Unknown endpoint!'})
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, req, res, next) => {
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  }
+  next(error)
+}
+
+
+app.use(errorHandler)
 app.listen(3001, () =>  {
   console.log('Server is running on port 3001')
 })
